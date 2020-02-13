30f138bf218ebca0fae2971269790fbc216f58b4
luanesouza <annie.usa25@gmail.com>
2020-02-12 20:52:16 -0500

    adds notes on server.js

--- backend/server.js
+++ backend/server.js
@@ -3,5 +3,12 @@ const bodyParser = require('body-parser')
 
 const app = express()
 
+app.use(bodyParser.json())
+
+
+// User >—UQ—< Quiz >- Questions -< Answers/Options
+//                                             |
+//                                             ^
+//                                          Explanation
 
 app.listen(5000)
